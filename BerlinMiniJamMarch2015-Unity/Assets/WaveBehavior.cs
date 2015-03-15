290e81d05064788b019249bdfa5c3ba53bd09de9
Viktor Pekar <vikpek@gmail.com>
2015-03-15 19:30:17 +0100

    pre merge commit n shit

---
+++ BerlinMiniJamMarch2015-Unity/Assets/WaveBehavior.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaveBehavior : MonoBehaviour {
+
+
+	int up = 1;
+
+	[SerializeField]
+	float movementSpeed = 10f;
+
+	[SerializeField]
+	float torqueSpeed = 10f;
+
+	[SerializeField]
+	float waveSize = 3f;
+
+	// Use this for initialization
+	void Start () {
+
+		InvokeRepeating("SwitchForce", 0f, waveSize);
+
+	}
+	
+	void SwitchForce()
+	{
+		GetComponent<ConstantForce2D>().relativeForce = (new Vector2(0f, movementSpeed * up));
+		GetComponent<ConstantForce2D>().torque = torqueSpeed * up;
+		up = up * -1;
+	}
+}
