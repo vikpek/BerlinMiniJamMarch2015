6a55df38189e98eb9ce3ebd71ca7ebe7c330fddf
Micha <michael_oelke@freenet.de>
2015-03-15 19:30:28 +0100

    startendnshot

---
+++ BerlinMiniJamMarch2015-Unity/Assets/LoadNext.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadNext : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void LoadMain(){
+		Application.LoadLevel("Main");
+	}
+	public void Close(){
+		//print ("");
+		Application.Quit();
+	}
+}
